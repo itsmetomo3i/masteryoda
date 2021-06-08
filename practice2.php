aafb3b82c8e034b6d2d9b61107e2c0e707065dcb
itsmetomo3i <tomm.k1030@gmail.com>
2021-06-08 18:29:16 +0000

    PHP/laravel 03 Kadai

--- practice2.php
+++ practice2.php
@@ -51,3 +51,42 @@ $a = 3;
 $b = 3;
 $c = "3";
 var_dump($a == $b);
+
+echo "If you view the page source \r\n you will find a newline in this string.";
+echo "<br>";
+echo nl2br("You will find the \n newlines in this string \r\n on the browser window." );
+$animals = array("dog","cat","panda","rabbit","squirrel");
+foreach($animals as $zoo){
+    echo "I have a" . "<br>" . $zoo;
+    echo "\n";
+}
+$name = "Bruce";
+if ($name == "tomomi") {
+    echo "MY NAME is $name desu";
+}
+else {
+    echo "I am not tomomi desu" ;
+    }
+echo "\n";
+for ($i = 1; $i < 10000; $i++){
+echo $i;
+}
+echo"\n";
+$fruit = array("peach","apple","orange");
+foreach ($fruit as $flu){
+    echo "I like $flu"."\n";
+}
+//課題４応用
+// for文の始めの値を定義する
+$start = 1;
+//* for文の終わりの値を定義する */
+$end = 100;
+
+for($i = $start; $i <= $end; $i++){
+
+  // 5で割り切れたら{}内を実行する
+if($i % 5 ==0 ){
+    echo $i . "\n";
+  }
+}
+echo "\n";
